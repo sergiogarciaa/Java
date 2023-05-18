5de4d1657a2581a545e341f8cf7834180a08c6ae
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-05-18 13:42:53 +0200

    Delete Ficheros/gestionGasolinera directory

--- Ficheros/gestionGasolinera/src/servicios/ImplementacionMenu.java
+++
@@ -1,9 +0,0 @@
-package servicios;
-
-public class ImplementacionMenu implements InterfazMenu {
-    public void mostrarMenu() {
-        System.out.println("0. Salir");
-        System.out.println("1. Registro");
-    }
-
-}
--- Ficheros/gestionGasolinera/src/servicios/InterfazUsuario.java
+++
@@ -1,7 +0,0 @@
-package servicios;
-import entidades.Usuarios;
-import  java.util.List;
-
-public interface InterfazUsuario {
-     public List<Usuarios> usuarios(List<Usuarios>listaUsuario);
-}
